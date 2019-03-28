e56d31657ee3fac74d578f862dc80c03ee50552f
Ethan Shen <ethanshen.wu@gmail.com>
2019-03-27 17:30:09 -0700

    job cards completed

--- src/JobCard.js
+++ src/JobCard.js
@@ -4,7 +4,12 @@ import React, { Component } from 'react';
 class JobCard extends Component {
     render () {
         return (
-            <div>tbd</div>
+            <div>
+                <h3>{ this.props.title }</h3>
+                <p>Salary: { this.props.salary }</p>
+                <p>Equity: { this.props.equity }</p>
+                <p>button should be here: { this.props.status }</p>
+            </div>
         )
     }
 }
