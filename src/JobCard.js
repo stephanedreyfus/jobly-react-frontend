3f53389ee286cabcd6ddcd8685f9cf782b6fd87b
Stéphane Dryefus <floatingrock@gmail.com>
2019-03-29 15:01:57 -0700

    Add try catch to JobCard handleUpdateUserJobs.

--- src/JobCard.js
+++ src/JobCard.js
@@ -4,6 +4,10 @@ import './JobCard.css';
 
 /** component shows details about a company */
 class JobCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {error: false}
+    }
 
     showApplyButton() {
         return (
@@ -14,24 +18,32 @@ class JobCard extends Component {
 
     showDisabledButton() {
         return (
-            <button className="apply-button"
+            <button className="apply-button disabled"
                 disabled={true}>Applied</button>
         );
     }
 
+    /** If parent list re-renders only re-renders card if
+     * this card changed. */
     shouldComponentUpdate(nextProps) {
         return this.props.state !== nextProps.state;
     }
 
 
-    /**  */
+    /** Adds job to applications table and sets state of job to
+     * "applied". If called through Jobs forces a render of the
+     * jobs page to update change in button. */
     async handleUpdateUserJobs() {
-        let {id, company_handle, title} = this.props
-        let job = {id, company_handle, title, state:"applied"};
-        let res = await JoblyApi.applyToJob(job.id);
-        this.props.updateUserJobs(job);
-        if (this.props.updateJobsList) this.props.updateJobsList(id);
-        return res;
+        try {
+            let {id, company_handle, title} = this.props
+            let job = {id, company_handle, title, state:"applied"};
+            let res = await JoblyApi.applyToJob(job.id);
+            this.props.updateUserJobs(job);
+            if (this.props.updateJobsList) this.props.updateJobsList(id);
+            return res;
+        } catch(err) {
+            this.setState({ error: true });
+        }
     }
 
     render() {
@@ -41,6 +53,9 @@ class JobCard extends Component {
                 <p>Salary: {this.props.salary}</p>
                 <p>Equity: {this.props.equity}</p>
                 {(this.props.state === null) ? this.showApplyButton() : this.showDisabledButton()}
+                {(this.state.error === true) ?
+                    <p>Something went wrong. Please try again later.</p> :
+                    null}
             </div>
         );
     }
